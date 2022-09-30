49bd4375dfc01cf8964881c18956530377a9ae24
abdoshy97 <abdoshy97@gmail.com>
2022-09-30 21:20:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import openpyxl
+
+
+st.set_page_config(page_title='Abdoshy App')
+st.title('Excal Mohamed 📊')
+st.subheader('this is test text')
+u_file=st.file_uploader('Choose a XLSX file',type='xlsx')
+if u_file:
+    st.markdown('---')
+    df=pd.read_excel(u_file,engine='openpyxl')
+    st.dataframe(df)
